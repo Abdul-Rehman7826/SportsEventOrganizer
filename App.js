accdf15f88c7df107af41c6f5fab030b642898f4
AbdulRehman <112686679+Abdul-Rehman7826@users.noreply.github.com>
2022-11-15 02:45:39 +0500

    modified:   App.js

--- App.js
+++ App.js
@@ -1,53 +1,19 @@
+import 'react-native-gesture-handler';
 import { useContext, useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import * as SecureStorage from 'expo-secure-store';
 import AppLoading from 'expo-app-loading';
+import * as SecureStorage from 'expo-secure-store';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import 'react-native-gesture-handler';
 import AuthContextProvider, { AuthContext } from './app/store/auth-context';
 import AuthNavigation from './app/Navigation/AuthNavigation';
-import AccountScreen from './app/screens/Profile/AccountScreen';
-import colors from './app/config/colors';
-import IconButton from './app/components/ui/IconButton';
-
-const Stack = createStackNavigator();
-
-function AuthenticatedStack() {
-
-  const authCtx = useContext(AuthContext);
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: colors.primary500 },
-        headerTintColor: 'white',
-        contentStyle: { backgroundColor: colors.black },
-      }}
-    >
-      <Stack.Screen
-        name="Welcome"
-        component={AccountScreen}
-        options={{
-          headerRight: ({ tintColor }) => (
-            <IconButton
-              icon="exit"
-              color={tintColor}
-              size={24}
-              onPress={authCtx.logout}
-            />
-          ),
-        }}
-      />
-    </Stack.Navigator>
-  );
-}
+import DrawerNavigation from './app/Navigation/DrawerNavigation';
 
 function Navigation() {
   const authCtx = useContext(AuthContext);
   return (
     <NavigationContainer>
       {!authCtx.isAuthenticated && <AuthNavigation />}
-      {authCtx.isAuthenticated && <postNavigation />}
+      {authCtx.isAuthenticated && <DrawerNavigation />}
     </NavigationContainer>
   );
 }
